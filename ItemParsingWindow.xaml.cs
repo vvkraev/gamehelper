f37b05e6e026efe2a4a2717c099971e97809d962
vvkraev <32893622+vvkraev@users.noreply.github.com>
2026-03-29 12:20:33 +0300

    Add code-behind for ItemParsingWindow

---
+++ ItemParsingWindow.xaml.cs
@@ -0,0 +1,20 @@
+// ItemParsingWindow.xaml.cs
+
+using System;
+using System.Windows;
+
+namespace GameHelper
+{
+    public partial class ItemParsingWindow : Window
+    {
+        public ItemParsingWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ParseItem(string itemData)
+        {
+            // Logic for parsing item data
+        }
+    }
+}
